592babf0918c0a42f43ba643d78be7b9b46b17ae
mundabenj <benjamin.mundama@strathmore.edu>
2025-10-03 10:44:55 +0300

    implememented git ignore

--- ClassAutoLoad.php
+++ ClassAutoLoad.php
@@ -1,4 +1,10 @@
 <?php
+
+// Check if configuration file exists
+if (!file_exists('conf.php')) {
+    die("Configuration file not found. Please create 'conf.php' from 'conf.sample.php'.");
+}
+
 require_once 'conf.php';
 
 $directories = [
